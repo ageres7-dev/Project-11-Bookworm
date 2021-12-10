1019d58a4b37a9e6694f2ede9ebf69881e22b2d9
Sergey Dolgikh <ageres7@gmail.com>
2021-12-11 00:49:05 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sergey Dolgikh on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
